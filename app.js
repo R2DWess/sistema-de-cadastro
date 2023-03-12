7e850cd5854b7db8549f7f80286161a96010fc7d
Joelma Giovana <127151616+JoTech001@users.noreply.github.com>
2023-03-12 15:27:46 -0300

    console log indicando modificacoes do paulo

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.loh("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+//mais um console de teste:
+console.log("O paulo fes esse commit sem voce ver na aula!")
