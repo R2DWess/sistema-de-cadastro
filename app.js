ce1d5eff937b70693cffe4e62fe6f5e0bc53c0e2
R2DWess <GusterLima@hotmail.com>
2023-03-12 15:34:09 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-console.loh("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao banco de dados")
+console.loh( "Rodando o sistema de gerenciamento de jogos" )
+console.log( "aplicação está se conectando ao banco de dados" )
 //mais um console de teste:
-console.log("A Joh fez esse commit sem voce ver!")
+console.log( "A Joh fez esse commit sem voce ver!" )
+console.log( "Enviando para o GitHub" )
