cbec744395e1a0fe88d44f992cae32371272d107
Joelma Giovana <127151616+JoTech001@users.noreply.github.com>
2023-03-12 15:29:10 -0300

    coonsole log indicando modificacoes da Joelma

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.loh("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 //mais um console de teste:
-console.log("O paulo fes esse commit sem voce ver na aula!")
+console.log("A Joelma fez esse commit sem voce ver!")
