657e409739320bd0d2fb46605a659b7f08bdb450
R2DWess <GusterLima@hotmail.com>
2023-03-12 15:45:26 -0300

    voltando para o estado da joh

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-console.loh( "Rodando o sistema de gerenciamento de jogos" )
-console.log( "aplicação está se conectando ao banco de dados" )
+console.loh("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
 //mais um console de teste:
-console.log( "A Joh fez esse commit sem voce ver!" )
-console.log( "Enviando para o GitHub" )
+console.log("A Joelma fez esse commit sem voce ver!")
